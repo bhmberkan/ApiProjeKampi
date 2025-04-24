a512725ae704c0ce0e93b212ae71ee6531b8acd8
bhmberkan <berkanburakturgut@gmail.com>
2025-04-24 17:07:27 +0300

    Product ve Category dtolarını oluşturdum. Menü için viewcomponentler yazdım Ve viewlarını oluşturdum. Ayrıca listeleme işlemlerini gerçekleştirdim.

---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DeaultMenuViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DeaultMenuViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
