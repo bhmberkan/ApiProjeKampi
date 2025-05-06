167ee78f2883a5ee70529cd3854351e0acde2cda
bhmberkan <berkanburakturgut@gmail.com>
2025-05-06 22:02:08 +0300

    Testminonial alanı için. Viewcomponent kullanarak ayrıştırma işlemini gerçekleştirdim. Bunun yanısıra. Api tarafında controller yazdım. ve ilgili verilerin. gösterilmesini sağladım.(api consume)
    AI kullanarak ornek promtlar oluşturdum ve sayfamda ilgili kısıma ekledim.
    
    viewımporst klasorune ilgili dtoyu geçtim.

---
+++ ApiProjeKampi.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,59 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public TestimonialsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _context.Testimonials.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateService(Testimonial testimonial)
+        {
+            _context.Testimonials.Add(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Ekleme işlemi başarılı");
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            _context.Testimonials.Remove(value);
+            _context.SaveChanges();
+            return Ok("Referans Silme İşlemi başarılı");
+        }
+
+        [HttpGet("GetService")] // iki tane get kullandık isimlendirme yapmalıyız
+        public IActionResult GetService(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateService(Testimonial testimonial)
+        {
+            _context.Testimonials.Update(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Güncelleme işlemi başarılı");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using ApiProjeKampi.WebUI.Dtos.CategoryDtos;
+using ApiProjeKampi.WebUI.Dtos.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _TestimonialDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client =_httpClientFactory.CreateClient();
+            var ResponseMessage = await client.GetAsync("https://localhost:7222/api/Testimonials");
+            if (ResponseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await ResponseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
