043f7c7b29af8f3dd3ea1fb06cc04ca6327cbaf6
bhmberkan <berkanburakturgut@gmail.com>
2025-10-09 00:32:29 +0300

    api tarafınsa statistics alanı yazıldı, feature alanındaki eksiklik giderildi. mig geçildi. home kısmında düzenlemeler. Header, about, menu gibi alanların yazılması

---
+++ ApiProjeKampi.WebApi/Migrations/20251008204730_mig2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiProjeKampi.WebApi.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoUrl",
+                table: "Features",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoUrl",
+                table: "Features");
+        }
+    }
+}
--- ApiProjeKampi.WebUI/ViewComponents/_AboutDefaultComponentPartial.cs
+++ ApiProjeKampi.WebUI/ViewComponents/_AboutDefaultComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi.WebUI.Dtos.AboutDtos;
+using ApiProjeKampi.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKampi.WebUI.ViewComponents
 {
     public class _AboutDefaultComponentPartial :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7222/api/Abouts/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
